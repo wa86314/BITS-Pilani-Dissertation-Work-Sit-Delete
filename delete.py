a0d46d1d6e885a697d957b050fd9f3524d43bf41
vboxuser <vboxuser@DevOpsLab.myguest.virtualbox.org>
2025-08-31 20:09:32 +0530

    Major Update

---
+++ delete.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+from connection import db_conn
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return "INFO: This Microservice is used for deleting record from Database"
+
+@app.route('/delete', methods=['DELETE'])
+def delete():
+    data = request.get_json()
+    stud_id = data.get('id')
+
+    db_cursor = db_conn.cursor()
+    db_cursor.execute("DELETE FROM student WHERE id = %s", (stud_id))
+    db_conn.commit()
+
+    #If the target ID won't present in the student table
+    if db_cursor.rowcount == 0:
+        return f"ID {stud_id} not found"
+
+    return f"Record deleted for {stud_id} successfully!"
+
+if __name__ == '__main__':
+     app.run(debug=True, port=9003, host='0.0.0.0')
+db_conn.close()
