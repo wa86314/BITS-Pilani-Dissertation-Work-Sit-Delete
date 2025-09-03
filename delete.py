e63d334842c7c937eedb871f9b4b20410c6e3ef3
wa86314 <2021wa86314@wilp.bits-pilani.ac.in>
2025-09-04 01:58:07 +0530

    Update delete.py

--- delete.py
+++ delete.py
@@ -23,5 +23,5 @@ def delete():
     return f"Record deleted for {stud_id} successfully!"
 
 if __name__ == '__main__':
-     app.run(debug=True, port=9003, host='0.0.0.0')
+     app.run(debug=True, port=5003, host='0.0.0.0')
 db_conn.close()
